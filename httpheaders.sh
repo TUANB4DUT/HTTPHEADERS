06f70490d1da2309027c826568f62744adcb2be1
TUAN B4DUT <41275808+TUANB4DUT@users.noreply.github.com>
2019-02-03 19:38:56 +0700

    Add files via upload

---
+++ httpheaders.sh
@@ -0,0 +1,20 @@
+#Hi! Ngapain exploit nano?
+#CODING SEDERHANA AJA MASA DI RECODE STER ^^
+#GUA EMANG GA PRO TAPI GUA BERUSAHA^^
+
+figlet -f slant TUAN B4DUT | lolcat
+
+echo  "_____________________________________________________________" |lolcat
+echo  "[TYPE      : TOOLS SCAN HTTP HEADER] " |lolcat
+echo  "[AUTHOR    : TUAN B4DUT] " |lolcat
+echo  "[ASSOCIATE : INDONESIAN TERMUX ASSOCIATION] " |lolcat
+echo  "[NOTES     : BE A SMART PEOPLE] " | lolcat
+echo  "[THANKS TO : GOD]" | lolcat
+echo  "_____________________________________________________________" |lolcat
+
+sleep 1
+
+echo  "${y} {1} MASUKAN WEB ADDRES / DOMAIN${endc}:" | lolcat
+read web
+curl https://api.hackertarget.com/httpheaders/?q=$web
+echo
